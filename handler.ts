44b484e803bdda03a7af98f69f11e38872a1f0aa
hong-brother <robbins.hs@gmail.com>
2023-09-13 00:09:05 +0900

    - 오늘 날짜 기준 몇일까지 조회 가능하도록 변경 - 단축 URL 기능 추가

--- handler.ts
+++ handler.ts
@@ -16,55 +16,85 @@ export const check: Handler = async (event: any, context: Context) => {
     };
 };
 
-async function getNews(keyword: string, start: number) {
+async function getNews(keyword: string, idx: number) {
     return await axios({
         method: 'get',
         url: 'https://openapi.naver.com/v1/search/news.json',
         params: {
             query: keyword,
             display: 100,
-            start,
+            start: idx,
             sort: 'date'
         },
         headers: {
-            "X-Naver-Client-Id" : process.env.clientId,
-            "X-Naver-Client-Secret": process.env.secret
+            "X-Naver-Client-Id" : process.env.NAVER_NEWS_CLIENT_ID,
+            "X-Naver-Client-Secret": process.env.NAVER_NEWS_SECRET_KEY
         }
     })
+}
+
+// @ts-ignore
+async function shortUrl(urlString: string) : Promise<string> {
+    return await axios({
+        method: 'get',
+        url: 'https://openapi.naver.com/v1/util/shorturl',
+        params: {
+          url: urlString
+        },
+        headers: {
+            "X-Naver-Client-Id" : process.env.NAVER_URL_CLIENT_ID,
+            "X-Naver-Client-Secret": process.env.NAVER_URL_SECRET_KEY
+        }
+    }).then(v=> {
+        if (v.data.code === "200") {
+            return v.data.result.url;
+        } else {
+            return "";
+        }
+    });
 }
 
 
 export const news: Handler = async (event: any, context: Context) => {
     console.log('call news');
     const queryStringParameters = event.queryStringParameters;
-    const startDate = dayjs(queryStringParameters['startDate']);
+    const keyword = event.queryStringParameters['keyword'];
+    const previousDate = Number(queryStringParameters['previousDate']);
+    const toDate = dayjs()
+    const previous = toDate.subtract(previousDate, 'day');
     // const endDate = dayjs(event.pathParameters.endDate);
     try{
         let arr = [];
-        let idx = 1;
-        const response = await getNews('관세청', idx);
-        for (let i = 0; i < response.data.items.length; i++) {
-            const t = dayjs(response.data.items[i].pubDate, {format: formatString}).format('YYYY-MM-DD');
-            if (dayjs(t).isBetween(startDate, startDate, null, '[]')) {
-                arr.push(response.data.items[i]);
-            } else {
-                break;
+        let idx = 1
+
+        while(true) {
+            let response = await getNews(keyword, idx);
+            for (let i = 0; i < response.data.items.length; i++) {
+                const t = dayjs(response.data.items[i].pubDate, {format: formatString}).format('YYYY-MM-DD');
+                if (dayjs(t).isBetween(toDate, previous, null, '[]')) {
+                    // response.data.items[i]['link'] = await shortUrl(response.data.items[i]['originallink']);
+                    arr.push(response.data.items[i]);
+                } else {
+                    idx = -1;
+                    break;
+                }
             }
+            if (idx === -1) break;
+            idx += 100;
         }
 
-
         // @ts-ignore
-        return template(startDate.format('YYYY-MM-DD'), arr);
+        return template(keyword, toDate.format('YYYY-MM-DD'), arr);
     }catch (e) {
         console.log(e)
-        return null;
+        return "server error";
     }
 }
 
-function template(toDate: string, arr: []) :String {
-    let str = `※ ${toDate} 주요기사(인천세관 홍보계)
+function template(keyword: string, period: string, arr: []) :String {
+    let str = `※ ${period} 주요기사(인천세관 홍보계)
 ` + '\n' + '\n' +
-        '- 관세청 및 세관 -';
+        `- ${keyword} -`;
     arr.forEach(v=> {
         str += v['title'] + '\n';
         str += v['link'] + '\n';
