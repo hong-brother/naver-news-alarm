06a9df254acc858b31658e4b6ea2a01f8a51c291
hong-brother <robbins.hs@gmail.com>
2023-10-07 01:04:45 +0900

    타입 오류 라이브러리 제거

--- news-web/src/app.middleware.ts
+++ news-web/src/app.middleware.ts
@@ -1,11 +1,11 @@
 import { INestApplication } from '@nestjs/common';
-import compression from 'compression';
+// import compression from 'compression';
 import cookieParser from 'cookie-parser';
 import { ConfigService } from '@nestjs/config';
 import helmet from 'helmet';
 
 export function middleware(app: INestApplication, config: ConfigService): INestApplication {
-  app.use(compression());
+  // app.use(compression());
   app.use(cookieParser());
 
   app.use(
--- news-web/src/common/utils/generator.service.ts
+++ news-web/src/common/utils/generator.service.ts
@@ -1,8 +1,7 @@
-import { Injectable } from '@nestjs/common';
-import { v4 as uuid } from 'uuid';
+// import { v4 as uuid } from 'uuid';
 
 export class GeneratorService {
   public uuid(): string {
-    return uuid();
+    return '';
   }
 }
