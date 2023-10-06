a43effca50adaed1d28bf99557ec3e4fdc934867
hong-brother <robbins.hs@gmail.com>
2023-10-06 23:49:41 +0900

    빌드 오류

--- web/src/server.ts
+++ web/src/server.ts
@@ -2,7 +2,7 @@ import { App } from '@/app';
 import { AuthRoute } from '@routes/auth.route';
 import { UserRoute } from '@routes/users.route';
 import { ValidateEnv } from '@utils/validateEnv';
-
+import 'module-alias/register';
 ValidateEnv();
 
 const app = new App([new UserRoute(), new AuthRoute()]);
